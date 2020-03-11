1d17dc0109da3f6efb86abebeb012b81d50ef34d
MeilyD07 <meilyd07@gmail.com>
2020-03-12 00:01:16 +0300

    feat: add test bill counter

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,6 +4,23 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
+    
+    NSUInteger count = bill.count;
+    if ((count > 1) && (index < count) && (index >= 0)) {
+        NSInteger totalSum = [[bill valueForKeyPath: @"@sum.self"] integerValue];
+        NSNumber *item = [bill objectAtIndex:index];
+        totalSum = totalSum - [item integerValue];
+        
+        double amount = totalSum / 2.0;
+        double charged = [sum doubleValue];
+        
+        if (charged == amount) {
+            return @"Bon Appetit";
+        } else {
+            return [NSString stringWithFormat:@"%g", (charged - amount)];
+        }
+    }
+    
     return @"";
 }
 
