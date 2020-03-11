add66f3a13b7f7d64e65e89b5c3317bfe10ffda7
MeilyD07 <meilyd07@gmail.com>
2020-03-11 22:16:20 +0300

    feat: add array test and miniMaxSum

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,40 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    
+    NSMutableArray *happyArray = [sadArray mutableCopy];
+    
+    BOOL isSadArray = TRUE;
+    
+    while (isSadArray) {
+        NSMutableIndexSet *badIndexesSet = [self getBadElementIndexes:happyArray];
+        if (badIndexesSet.count > 0) {
+            [happyArray removeObjectsAtIndexes:badIndexesSet];
+        } else {
+            isSadArray = NO;
+        }
+    };
+    
+    return happyArray;
+}
+
+- (NSMutableIndexSet *)getBadElementIndexes: (NSArray *) sadArray {
+    NSMutableIndexSet *badIndexes = [NSMutableIndexSet new];
+    
+    NSUInteger count = [sadArray count];
+    if (count > 0) {
+        for (int index = 1; index < count - 1; index++) {
+            id item = [sadArray objectAtIndex:(index)];
+            NSNumber *previousItem = [sadArray objectAtIndex:(index - 1)];
+            NSNumber *nextItem = [sadArray objectAtIndex:index + 1];
+            
+            if ([previousItem intValue] + [nextItem intValue] < [item intValue]) {
+                [badIndexes addIndex:index];
+            }
+        }
+    }
+    
+    return badIndexes;
 }
 
 @end
--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,12 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    
+    NSNumber *totalSum = [array valueForKeyPath:@"@sum.self"];
+    NSArray<NSNumber*>* sortedArray = [array sortedArrayUsingSelector:@selector(compare:)];
+    NSInteger minimumSum = [totalSum intValue] - [sortedArray.lastObject intValue];
+    NSInteger maximumSum = [totalSum intValue] - [sortedArray.firstObject intValue];
+    return @[[NSNumber numberWithInteger:minimumSum], [NSNumber numberWithInteger:maximumSum]];
 }
 
 @end
