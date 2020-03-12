ea781d75c0ba99a32f69d96cc6d8341752ac92f9
MeilyD07 <meilyd07@gmail.com>
2020-03-13 02:25:15 +0300

    fix: change test parser

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,47 +4,74 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
+    NSMutableArray *arrayOfPairs = [NSMutableArray new];
     
-    NSMutableArray *arrayOfStrings = [[NSMutableArray alloc] init];
-  
-    
-    NSArray <NSString*> *patterns = @[@"<.*?>", @"<.*>", @"\\[.*?\\]",@"\\[.*\\]", @"\\(.*?\\)"];
-    //NSArray <NSString*> *patterns = @[@"<.*?>", @"\\[.*?\\]", @"\\(.*?\\)"];
-    
-    for (NSString *pattern in patterns) {
+    NSArray *charSetArrays = @[@[@"<", @">"],
+                               @[@"(", @")"],
+                               @[@"[", @"]"]
+    ];
+
+    for (NSArray *charSetArray in charSetArrays) {
+        
         NSError *error = nil;
+        
+        NSMutableArray *firstCharPositions = [NSMutableArray new];
+        NSString *pattern = [NSString stringWithFormat:@"\\%@", charSetArray[0]];
         NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:&error];
         NSArray<NSTextCheckingResult *> *matches = [regex matchesInString:string options:0 range:NSMakeRange(0, string.length)];
-
+        
         for (NSTextCheckingResult *match in matches) {
-            NSString* substringForMatch = [string substringWithRange:match.range];
+            [firstCharPositions addObject: [NSNumber numberWithUnsignedInteger:match.range.location]];
+        }
+        
+        NSMutableArray *secondCharPositions = [NSMutableArray new];
+        pattern = [NSString stringWithFormat:@"\\%@", charSetArray[1]];
+        regex = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:&error];
+        matches = [regex matchesInString:string options:0 range:NSMakeRange(0, string.length)];
+        
+        for (NSTextCheckingResult *match in matches) {
+            [secondCharPositions addObject: [NSNumber numberWithUnsignedInteger:match.range.location]];
+        }
+        
+        for (int i = 0; i < firstCharPositions.count; i++) {
+            NSUInteger firstIndex = ((NSNumber *)(firstCharPositions[i])).unsignedIntegerValue;
             
-            NSLog(@"Extracted: %@, %lu",substringForMatch, (unsigned long)match.range.location);
-
-            NSArray *pair = @[
-                @(match.range.location),
-                [[substringForMatch substringFromIndex:1] substringToIndex:substringForMatch.length - 2]];
-            
-            if (![arrayOfStrings containsObject:pair]) {
-                [arrayOfStrings addObject:pair];
+            for (int j = 0; j < secondCharPositions.count; j++) {
+                NSUInteger secondIndex = ((NSNumber *)(secondCharPositions[j])).unsignedIntegerValue;
+                
+                if (firstIndex < secondIndex) {
+                    NSString *tempString = [string substringWithRange:NSMakeRange(firstIndex + 1, secondIndex - firstIndex - 1)];
+                    if ([tempString containsString:charSetArray[0]] && ![tempString containsString:charSetArray[1]]) {
+                        continue;
+                    } else {
+                        
+                        NSArray *pair = @[
+                            firstCharPositions[i],
+                            tempString];
+                        
+                        [arrayOfPairs addObject:pair];
+                        break;
+                    }
+                } else {
+                    continue;
+                }
             }
         }
     }
     
-    NSArray *sortedArray = [arrayOfStrings sortedArrayUsingComparator:^NSComparisonResult(id a, id b) {
+    NSArray *sortedArray = [arrayOfPairs sortedArrayUsingComparator:^NSComparisonResult(id a, id b) {
         NSNumber *first = [(NSArray *)a objectAtIndex:0];
         NSNumber *second = [(NSArray *)b objectAtIndex:0];
         
         return [first compare:second];
     }];
     
-    NSMutableArray *tempResults = [NSMutableArray new];
+    NSMutableArray <NSString*> *arrayOfStrings = [[NSMutableArray alloc] init];
     for (NSArray *item in sortedArray) {
-        
-        [tempResults addObject:item[1]];
-    }
+           [arrayOfStrings addObject:item[1]];
+       }
     
-    return [NSArray arrayWithArray:tempResults];
+    return [NSArray arrayWithArray:arrayOfStrings];
 }
 
 @end
